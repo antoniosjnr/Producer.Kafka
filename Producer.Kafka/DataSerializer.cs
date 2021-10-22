b5d4952b47d0b311d06bfadedeb9a432da3783df
antonio.junior <antonio.sjnr@gmail.com>
2021-10-22 17:15:28 -0300

    Ajustes relacionados a Serialização de objetos

---
+++ Producer.Kafka/DataSerializer.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+namespace Producer.Kafka
+{
+    public class DataSerializer<T> : ISerializer<T>
+    {
+        public byte[] Serialize(T data, SerializationContext context)
+        {
+            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data));
+        }
+    }
+}
---
+++ Producer.Kafka/Order.cs
@@ -0,0 +1,8 @@
+﻿namespace Producer.Kafka
+{
+    public class Order
+    {
+        public string Number { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
